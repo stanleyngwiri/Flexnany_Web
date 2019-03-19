3acd1b154eddf215fabea37e46219b1d63a21633
Wess58 <wesleymbate@gmail.com>
2019-03-19 15:30:44 +0300

    added var for data being stored

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,31 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var db = firebase.firestore();
+
 
 function storeData() {
+
+  var firstname = document.getElementById("firstname").value;
+  var lastname = document.getElementById("lastname").value;
+  var phone_no = document.getElementById("phone_no").value;
+  var location = document.getElementById("location").value;
+  var ratings = document.getElementById("ratings").value;
+  var description = document.getElementById("description").value;
+
   // Add a new document in collection "cities"
-  db.collection("cities").doc("LA").set({
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA"
-  })
-  .then(function() {
+  db.collection("Maids").doc().set({
+      firstname: firstname,
+      lastname: lastname,
+      phone_no: phone_no,
+      location: location,
+      ratings: ratings,
+      description: description
+    })
+    .then(function() {
       console.log("Document successfully written!");
-  })
-  .catch(function(error) {
+    })
+    .catch(function(error) {
       console.error("Error writing document: ", error);
-  });
+    });
 }
